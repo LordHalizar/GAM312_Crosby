176cc334b90d5417013a554cd747b8ece35d6b32
LordHalizar <crosbyalexander42@protonmail.com>
2025-03-04 22:34:27 -0700

    Created Player Character & Movement
    Setup basic character model, movement, mouse look, and jump action.

---
+++ Source/GAM312_Crosby/GAM312PrjctGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GAM312PrjctGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM312_CROSBY_API AGAM312PrjctGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
